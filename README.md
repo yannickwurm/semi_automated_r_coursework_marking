339c7c7ad6a389b76b1b61e85a089742d18ecbba
Yannick Wurm <y.wurm@qmul.ac.uk>
2024-11-28 11:03:27 +0000

    better: numbers, cleanup, smaller font

--- README.md
+++ README.md
@@ -4,23 +4,33 @@ Check naming matches what it did in 2022 - see #2
 
 ** NO THIS IS RWONG> THE NUMBERS ARE NOT STUDENT IDS... I DONT KNOW A GOOD WAY OF GETTING THEM> SO I SEPARATELY DOWNLOAD STUDENT NAMES AND USE THOSE TO GET THEIR EMAISL WHICH IS A PRIMARY KEY FOR UPLOAD>>>>
 
-# Do the marking within git/code directory 
+# Do the marking within git/code directory
 
-Download all submissions, rename overall dear to submissions, raname each student dir to only be studnet number
+Download all submissions, rename overall dear to submissions, rename each student dir to only be btxxxx_studnetid
 
 ```
-mv BIO773-Downloaded_directory_with_long_names submissions
-cd submission
-# keep only numbers
-rename --dry-run -e 's/([A-z_\- ]*)([0-9]+)(_[A-z]+)/$2/'  * 
+cp -r BIO773P\ -\ 202425* submissions
+rename --dry-run -e 's/(Participant_)([a-z]+[0-9]+)_studentid_([0-9]+)(_assign.*)/$2/'  submissions/*
+rename  -e 's/(Participant_)([a-z]+[0-9]+)_studentid_([0-9]+)(_assign.*)/$2/'  submissions/*
 
-rename  -e 's/([A-z_\- ]*)([0-9]+)(_[A-z]+)/$2/'  * 
+```
+
+```R
+# reqd' packaged
+install.packages("testthat")
+install.packages("lintr")
+install.packages("stringi")
+install.packages("stopwords")
+install.packages("stringr")
+install.packages("rmarkdown")
+install.packages("tm")
+install.packages("tokenizers")
 ```
 
 
 Then I ran marking code on each one withing a reporting environment, as part of `mark_text_shortener.Rmd`:
 
-```bash 
+```bash
 ls submissions | parallel "cd submissions/{}; ln -s ../../mark_text_shortener.Rmd"
 ls submissions | parallel "cd submissions/{}; ln -s ../../test_text_shortener.R"
 
@@ -28,9 +38,9 @@ ls submissions | parallel -j 1 -t -q --workdir submissions/{} Rscript -e 'librar
 
 ```
 
-Running typically requires installing all the R libraries that students depend on. 
+Running typically requires installing all the R libraries that students depend on.
 
-I had to check for "Error"  `Execution halted` messages that occurred when students had superfluous commands in their code (e.g. calling variable that don't exist, etc). **When this happens, add a `additional_notes.txt` into the student's directory.**
+I had to check for "quit" "Error"  `Execution halted` messages that occurred when students had superfluous commands in their code (e.g. calling variable that don't exist, etc). **When this happens, add a `additional_notes.txt` into the student's directory.**
 
 
 (This creates a detailed html doc for each student, highlighting which tests failed and which worked - thanks to R's testthat unit testing package)
@@ -40,16 +50,16 @@ I had to check for "Error"  `Execution halted` messages that occurred when stude
 To populate my excel with the students marks:
 ```
 mkdir marks
-egrep "successfu" submissions/*/*.md | sed 's/submissions\///' | sed 's/\/mark_text_shortener.md:I performed/,/' | sed 's/ of the code functionality. Among these, /,/' | sed 's/ passed successfully. There were /, passed, /' > marks/test_per_student.csv
-grep "issues with style" submissions/*/*.md | ruby -pe 'gsub(/[A-z.:,.\/]+/, "")' | ruby -pe 'gsub(/ +/, " ")' | cut  -f1,2 > marks/lint_issues_per_student.tsv
+ egrep "I performed" submissions/*/*.md | sed -n 's/submissions\/\(.*\)\/mark.*performed \([0-9]*\) tests.*,\(.*\) passed.*/\1 \2 \3/p' > marks/test_per_student.tsv
+egrep "lintr\` found" submissions/*/*.md | sed -n 's/submissions\/\(.*\)\/mark_text_shortener.md:Overall, `lintr` found \([0-9]*\) issues.*/\1 \2/p'  > marks/lint_issues_per_student.tsv
 
 ```
-Merged results & added calculations in Numbers & uploaded (by pasting) from there. 
+Merged results & added calculations in Numbers & uploaded (by pasting) from there.
 
 
-For sharing these results with students, I did: 
+For sharing these results with students, I did:
 
 ```
-ls submissions |  parallel -j 1 -t "scp submissions/{}/mark_text_shortener.html ubuntu@sequenceserver.com://efs/static-sites/antgenomes.org/users/yannickwurm/tmp/feedback/2021_{}.html"
+ls submissions |  parallel -j 1 -t "scp submissions/{}/mark_text_shortener.html ubuntu@sequenceserver.com://efs/static-sites/antgenomes.org/users/yannickwurm/tmp/feedback/2023_{}.html"
 ```
 
--- test_text_shortener.R
+++ test_text_shortener.R
@@ -1,51 +1,52 @@
 library(testthat)
-context("Question 1: shorten_text")
+context("Automatically testing the shorten_text function")
 
-test_that(desc = "Does the function shorten_text exist?",
+
+test_that(desc = "1. Does the function shorten_text exist?",
           code = {
             expect(exists("shorten_text"),
                    failure_message = "No function found with that name.")
           })
 
-test_that(desc = "Does it run and appropriately return a short input unchanged?",
+test_that(desc = "2. Does it run and appropriately return a short input unchanged?",
           code = {
             expect_equal(object   = shorten_text("asdf"),
                          expected = "asdf")
           })
 
-test_that(desc = "Function handles sentence without changes or full stops appropriately",
+test_that(desc = "3. Function handles sentence without changes or full stops appropriately",
           code = {
             text <- "This is a text with no full stops"
             expected <- "This is a text with no full stops"
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Function returns an empty string when given an empty string",
+test_that(desc = "4. Function returns an empty string when given an empty string",
           code = {
             expect_equal(shorten_text(""), "")
           })
 
-test_that(desc = "Replace multiple consecutive spaces by a single space: fish",
+test_that(desc = "5. Replace multiple consecutive spaces by a single space: fish",
           code = {
             expect_equal(object   = shorten_text("red   fish blue fish."),
                          expected = "red fish blue fish.")
           })
 
-test_that(desc = "Multiple consecutive spaces are replaced by a single space",
+test_that(desc = "6. Multiple consecutive spaces are replaced by a single space",
           code = {
             text <- "This is  a  test    with     space."
             expected <- "This is a test with space."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Replace multiple consecutive spaces by a single space and an exclamation",
+test_that(desc = "7. Replace multiple consecutive spaces by a single space and an exclamation",
           code = {
             text <- "Hello   world!"
             expected <- "Hello world!"
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Replace multiple consecutive spaces by a single space: ants are best",
+test_that(desc = "8. Replace multiple consecutive spaces by a single space: ants are best",
           code = {
             text <- "Yes  maam  ants  are  best  sir."
             expected <- "Yes maam ants are best sir."
@@ -53,91 +54,91 @@ test_that(desc = "Replace multiple consecutive spaces by a single space: ants ar
           })
 
 
-test_that(desc = "Function does not abbreviate 5-character words",
+test_that(desc = "9. Function does not abbreviate 5-character words",
           code = {
             text <- "Words like apple and water may stay."
             expected <- "Words like apple and water may stay."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Abbreviate words longer than 5 chars (i.e. replace extra characters by a '.'): Lala",
+test_that(desc = "10. Abbreviate words longer than 5 chars (i.e. replace extra characters by a '.'): Lala",
           code = {
             text <- "Lalalalalalalalala"
             expected <- "Lalal."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Words longer than 5 characters are abbreviated",
+test_that(desc = "11. Words longer than 5 characters are abbreviated",
           code = {
             text <- "Programming in R is interesting."
             expected <- "Progr. in R is inter.."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Abbreviate words longer than 5 chars (i.e. replace extra characters by a '.'): expectation",
+test_that(desc = "12. Abbreviate words longer than 5 chars (i.e. replace extra characters by a '.'): expectation",
           code = {
             text <- "Expectation, anticipation, gravity & hope."
             expected <- "Expec., antic., gravi. & hope."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Remove all of the following words: 'the', 'really', 'truly', 'very'.",
+test_that(desc = "13. Remove all of the following words: 'the', 'really', 'truly', 'very'.",
           code = {
             text <- "We go hard or we go home We gon do this all very night long"
             expected <- "We go hard or we go home We gon do this all night long"
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Remove 'the'.",
+test_that(desc = "14. Remove 'the'.",
           code = {
             test <- "Say hello to the big world"
             expected <- "Say hello to big world"
             expect_equal(shorten_text(test), expected)
           })
 
-test_that(desc = "Remove 'the' (twice).",
+test_that(desc = "15. Remove 'the' (twice).",
           code = {
             test <- "Say hello to the world the big world"
             expected <- "Say hello to world big world"
             expect_equal(shorten_text(test), expected)
           })
 
-test_that(desc = "Remove 'the' and 'very'; adjust spacing.",
+test_that(desc = "16. Remove 'the' and 'very'; adjust spacing.",
           code = {
             test <- "Say hello to the world the very big world"
             expected <- "Say hello to world big world"
             expect_equal(shorten_text(test), expected)
           })
 
-test_that(desc = "Remove: 'the', 'really', 'truly', 'very' and adjust space.",
+test_that(desc = "17. Remove: 'the', 'really', 'truly', 'very' and adjust space.",
           code = {
             text <- "Hey really truly very the bee you know me."
             expected <- "Hey bee you know me."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Remove: 'really' at end of sentence",
+test_that(desc = "18. Remove: 'really' at end of sentence",
           code = {
             text <- "I love R really."
             expected <- "I love R."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Remove all of the following words: 'really', 'truly', 'very'.",
+test_that(desc = "19. Remove all of the following words: 'really', 'truly', 'very'.",
           code = {
             text <- "Really big ants are truly cool and really easy to look at."
             expected <- "Big ants are cool and easy to look at."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Function abbreviates even if word has punctuation",
+test_that(desc = "20. Function abbreviates even if word has punctuation",
           code = {
            text <- "A functionality's details are specific."
            expected <- "A funct.'s detai. are speci.."
            expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Understand that an exclamation mark ends a sentence",
+test_that(desc = "21. Understand that an exclamation mark ends a sentence",
           code = {
             text <- "Ah! I hate edge cases."
             expected <- "Ah!"
@@ -145,56 +146,56 @@ test_that(desc = "Understand that an exclamation mark ends a sentence",
           })
 
 
-test_that(desc = "Specified words are removed, and capitalisation adjusted",
+test_that(desc = "22. Specified words are removed, and capitalisation adjusted",
           code = {
             text <- "The sky is really blue and truly blue."
             expected <- "Sky is blue and blue."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Function does not remove words that contain 'the' as substring",
+test_that(desc = "23. Function does not remove words that contain 'the' as substring",
           code = {
             text <- "Theme of the meeting was thermal energy."
             expected <- "Theme of meeti. was therm. energ.."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Keep only the first sentence in every paragraph if one paragraph.",
+test_that(desc = "24. Keep only the first sentence in every paragraph if one paragraph.",
           code = {
             expect_equal(object   = shorten_text("Long hair. Long life."),
                          expected = "Long hair.")
           })
 
-test_that(desc = "Function handles single-word paragraphs",
+test_that(desc = "25. Function handles single-word paragraphs",
           code = {
             text <- "Hello.\n\nWorld."
             expected <- "Hello.\n\nWorld."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Function maintains paragraph structure with multiple spaces in one-sentence paragraphs.",
+test_that(desc = "26. Function maintains paragraph structure with multiple spaces in one-sentence paragraphs.",
           code = {
-            text <- "This is  a  test.    \n\nThis is only a   test with  spaces."
-            expected <- "This is a test.\n\nThis is only a test with spaces."
+            text <- "This is  a  test.    \n\nThis is only a   test with  space."
+            expected <- "This is a test.\n\nThis is only a test with space."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Keep only the first sentence in every paragraph, if multiple paragraphs are present.",
+test_that(desc = "27. Keep only the first sentence in every paragraph, if multiple paragraphs are present.",
           code = {
             text <-
-              "This is the first sentence. This is the second.\n\nThis is the third. This is the fourth."
-            expected <- "This is the first sentence.\n\nThis is the third."
+              "This is first one. This is second.\n\nThis is third. This is fourth."
+            expected <- "This is first one.\n\nThis is third."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Keep only the first sentence, when several other changes are made.",
+test_that(desc = "28. Keep only the first sentence, when several other changes are made.",
           code = {
             text <-  "I - confine myself to one special difficulty, which at first appeared to me insuperable, and actually fatal to my whole theory. I allude to the neuters or sterile females in insect-communities: ... sterile ants."
             expected <- "I - confi. mysel. to one speci. diffi., which at first appea. to me insup., and actua. fatal to my whole theor.."
             expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Keep only the first sentence in every paragraph when no other changes.",
+test_that(desc = "29. Keep only the first sentence in every paragraph when no other changes.",
           code = {
             text <-
               paste(
@@ -211,7 +212,7 @@ test_that(desc = "Keep only the first sentence in every paragraph when no other 
               expect_equal(shorten_text(text), expected)
           })
 
-test_that(desc = "Function maintains special characters in text",
+test_that(desc = "30. Function maintains special characters in text",
           code = {
             text <- "Wow: !@#$ are same same."
             expected <- "Wow: !@#$ are same same."
